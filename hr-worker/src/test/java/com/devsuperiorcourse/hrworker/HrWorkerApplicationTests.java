b386ec9cb2dae43ffec10e2ff9c593ea6e1629ce
João Victor Marques Ferreira <joao.marques@castgroup.com.br>
2021-08-15 03:07:10 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/devsuperiorcourse/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorcourse.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
