c081167da5ac357fd03c1040315eed2b0a6e257a
jjoao-marques <joaomarques.jm991@gmail.com>
2021-08-19 23:28:54 -0300

    Project hr-user created

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.devsuperior.hrconfigserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,9 +8,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
+	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
+
+	
 
 }
