1982c42e8baff4afd37dafc6a7535d1d8f4b152a
jjoao-marques <joaomarques.jm991@gmail.com>
2021-08-21 00:10:59 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeignClients;
+
+@Service
+public class UserService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClients userFeignClients;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClients.findByEmail(email).getBody();
+		if (user == null ) {
+			logger.error("Email not found " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email Found " + email);
+		return user;
+	}
+
+}
